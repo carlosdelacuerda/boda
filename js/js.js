a42106e86e22d87b1d6c1f6cfc250eb94f931c55
Carlos de la Cuerda Gómez <carlosdelacuerda@gmail.com>
2021-12-22 22:52:32 +0100

    Editar lista de invitados

--- js/js.js
+++ js/js.js
@@ -3,5 +3,14 @@ var image = document.getElementsByClassName('thumbnail');
 new simpleParallax(image, {
 	delay: '0',
     orientation: 'down',
-    scale: '2'
+    scale: '1.3'
 });
+
+// modal
+const modal = document.getElementById("modal")
+function openModal () {
+    modal.classList.add("d-block")
+}
+function closeModal () {
+    modal.classList.remove("d-block")
+}
