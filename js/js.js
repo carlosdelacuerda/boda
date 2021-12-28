68894a66f993a6a28b60e4c2605142b8bf88e4bc
Carlos de la Cuerda Gómez <carlosdelacuerda@gmail.com>
2021-12-28 22:30:34 +0100

    galería de imágenes

--- js/js.js
+++ js/js.js
@@ -14,3 +14,27 @@ function openModal () {
 function closeModal () {
     modal.classList.remove("d-block")
 }
+
+// gallery 
+let pictures = new Array
+const requestURL = 'http://127.0.0.1:5500/images.json'
+const request = new XMLHttpRequest();
+request.open('GET', requestURL)
+request.responseType = 'json'
+request.send()
+request.onload = function () {
+    pictures = request.response
+    createGallery()
+}
+function createGallery () {
+    const picturesLenght = pictures.length
+    for (let i = 0; i < picturesLenght; i++) {
+        placeImages(pictures[i])
+    }
+}
+function placeImages (image) {
+    let thisLi = document.createElement(`li`)
+    thisLi.innerHTML = `<img src=${image.img} />`
+    const placeThisImage = document.getElementById('gallery')
+    placeThisImage.appendChild(thisLi)
+}
